9166fafa07d17645aa2a9a9c526b30681fc28a8f
Santiago Casas <santiagocasas@gmail.com>
2024-09-27 10:24:24 +0200

    changes to improve Freeze help and move code_versions one level up

--- topo/FreezeAnalysis.py
+++ topo/FreezeAnalysis.py
@@ -1,35 +1,30 @@
 from eth_account import Account
 from utils import (
-    load_private_key, compute_sha256,  
+    load_private_key, compute_sha256,
     compute_analysis_hash,
-    sign_proof_object,  
+    sign_proof_object,
     save_proof_and_signatures_json
 )
-import sys 
+import sys
 import os
+import argparse
 
-if __name__ == "__main__":
-    """
-    Main function to orchestrate the hash generation and display the analysis information.
-    """
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Generate and sign analysis hash for input file.")
+    parser.add_argument("input_path", help="Path to the cobaya input file for analysis. Example: scripts/my_input.yaml")
+    parser.add_argument("extra_args", nargs="*", help="Path to the code versions .json file that specifies the theory code used. Example: topo/code_versions.json")
+    return parser.parse_args()
 
-    try:
-        input_path = sys.argv[1]  # First argument is expected to be the input file path
-    except IndexError:
-        print("Please specify the location of the input file.")
-        sys.exit(1)  # Exit with a non-zero status to indicate an error
-
-    # Collect any extra arguments beyond the input path
-    
-    extra_args = sys.argv[2:]  
-
-
+def main(args):
     # Load private key and derive public key
     try:
         if os.path.exists("topo/cryptoFiles/private_key.txt"):
             private_key = load_private_key("topo/cryptoFiles/private_key.txt")
         elif os.path.exists("topo/cryptoFiles/encrypted_key.json"):
             private_key = load_private_key("topo/cryptoFiles/encrypted_key.json")
+        else:
+            print("No private key found. You can run Keygen.py to create a keypair.")
+            sys.exit(1)
     except FileNotFoundError:
         print("No private key found. You can run Keygen.py to create a keypair.")
         sys.exit(1)  # Exit the program with a non-zero status to indicate failure
@@ -42,7 +37,7 @@ if __name__ == "__main__":
     account = Account.from_key(private_key)
 
     # Compute analysis hash and sign the pre-object
-    pre_object, _ = compute_analysis_hash(input_path,extra_args)
+    pre_object, _ = compute_analysis_hash(args.input_path, args.extra_args)
     pre_hash, signature = sign_proof_object(private_key, pre_object)
 
     # Verify the signature
@@ -62,6 +57,8 @@ if __name__ == "__main__":
     save_proof_and_signatures_json(f'topo/cryptoFiles/pre_object_{pre_hash[:6]}.json', pre_object, signature, None, public_key)
     print('Pre-object saved in JSON')
 
-
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
 
 
