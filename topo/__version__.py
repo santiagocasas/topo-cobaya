8daffaeea9b239a6857f18d9b4e0480c72a5bb1e
Santiago Casas <santiagocasas@gmail.com>
2024-10-29 14:24:06 +0100

    version

---
+++ topo/__version__.py
@@ -0,0 +1,7 @@
+# Version for TOPO-Cobaya
+__version__ = "0.2.0"   
+
+#MAJOR.MINOR.PATCH
+#MAJOR: breaking changes
+#MINOR: new features, but backwards compatible
+#PATCH: backwards compatible bug fixes
